b5461b990a779e121975ef9359446a82b4f84f56
Brittni Carter <bfcarter@fullsail.edu>
2017-03-12 21:33:47 -0500

    created test.js

---
+++ test/test.js
@@ -0,0 +1,9 @@
+const assert =require ('assert');
+const Randomizer = require('../randomizer.js');
+const sequelize =require('sequelize');
+const sequelize = new sequelize('bfcarter_shortner',
+'bfcarter_shorten', 'c92kfZmdlamn@2d', {
+host: '212.1.210.1',
+dialect: 'mysql',
+});
+
