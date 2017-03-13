81aed6ee39c67fb6df42fc1a28ff769491e3b6a7
Brittni Carter <bfcarter@fullsail.edu>
2017-03-12 21:51:09 -0500

    more to test + comments

--- test/test.js
+++ test/test.js
@@ -6,4 +6,49 @@ const sequelize = new sequelize('bfcarter_shortner',
 host: '212.1.210.1',
 dialect: 'mysql',
 });
+//url shortner
+const Url = sequelize.define('url', {
+    short: {
+        type: sequelize.string,
+    },
+    long: {
+        type:sequelize.string,
+    }
+}, {
+    freezeTableName: true
+});
 
+describe('urls', () => {
+  describe('/api/makeurl', () => {
+    it('should return random url id', () => {
+      const random = Randomizer();
+      const longurl = 'https://google.com';
+
+      Url.create({
+        short: random,
+        long: longurl,
+      });
+
+      assert.equal(true, random.length > 0);
+    });
+  });
+  describe('/api/gourl/:short', () => {
+    it('should find the destination url', (done) => {
+      const random = Randomizer();
+      const longurl = 'https://google.com';
+//create url
+      Url.create({
+        short: random,
+        long: longurl,
+      }).then(() => {
+        Url.findOne({
+          where: {
+            short: random,
+          },
+        }).then(() => {
+          done();
+        });
+      });
+    });
+  });
+}
